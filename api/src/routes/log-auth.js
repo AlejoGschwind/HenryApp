f798e139e249044e79ca34e75b21c2dcc8d5d0fb
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-01-24 15:45:21 -0300

    notas sobre rutas comentadas

--- api/src/routes/log-auth.js
+++ api/src/routes/log-auth.js
@@ -3,13 +3,22 @@ const router = express.Router();
 
 const passport = require('passport');
 
+
+//--------------------------------------------------------------------------------
+//----Estas rutas pedia rendrizar un archivo de views -> innecesario y eliminado
 // router.get('/', (req, res, next) => {
-//   res.render('index');
+//   res.render('index'); //<-- este index era de views
 // });
 
 // router.get('/register', (req, res, next) => {
-//   res.render('register');
+//   res.render('register'); //<-- este register era de views
 // });
+
+// router.get('/login', (req, res, next) => {
+//   res.render('login'); //<-- este login era de views
+// });
+// igual lo dejo comentado por si sirve de algo
+//-------------------------------------------------------------------------------
 
 router.post('/register', passport.authenticate('local-register', {
   successRedirect: '/profile',
@@ -17,9 +26,7 @@ router.post('/register', passport.authenticate('local-register', {
   passReqToCallback: true
 }));
 
-// router.get('/login', (req, res, next) => {
-//   res.render('login');
-// });
+
 
 router.post('/login', passport.authenticate('local-login', {
   successRedirect: '/profile',
@@ -39,9 +46,11 @@ router.get('/logout', (req, res, next) => {
   res.redirect('/');
 });
 
+//--------Esta ruta estaba puesta aca porque estaba autenticada------------
 // router.get('/profile', (req, res, next) => {
-//   res.render('profile');
-// });
+//   res.render('profile');                 //<-- este profile era de views
+// });     
+//-------------------------------------------------------------------------
 
 
 //----funcion de autenticacion
